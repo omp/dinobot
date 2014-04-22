1effce31316dff82452990123729791c6d3daf63
David Vazgenovich Shakaryan <dvshakaryan@gmail.com>
2014-04-22 05:14:40 -0700

    Add more informative example.

--- example.rb
+++ example.rb
@@ -5,7 +5,16 @@ require_relative 'dinobot'
 bot = Dinobot::Bot.new('irc.example.org', 6667, 'dinobot') do
   join '#dinobot'
 
+  load_module :admin
+  load_module :config
+  load_module :lastfm
   load_module :test
+
+  @modules[:admin].add('dinobot.example.org')
+  @modules[:lastfm].setapikey('keyhere')
+
+  add_alias 'np', 'lastfm np'
+  add_alias 'song', 'lastfm song'
 end
 
 loop do
